c67a63325094c15c18d5e66af1c441e870a498f5
Sergio Cruz <cruzmosergio@gmail.com>
2024-11-30 12:42:33 +0000

    Add loading screen

--- app/src/main/java/com/grupo1/lojasocial/ui/theme/Theme.kt
+++ app/src/main/java/com/grupo1/lojasocial/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.grupo1.lojasocial.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,29 +8,27 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = primaryFont,
+    onPrimary = Color.White,
+    secondary = secondaryFont,
+    onSecondary = Color.Black,
+    background = primaryBackground,
+    onBackground = Color.Black
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    primary = primaryFont,
     onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    secondary = secondaryFont,
+    onSecondary = Color.Black,
+    background = primaryBackground,
+    onBackground = Color.Black
 )
+
 
 @Composable
 fun LojaSocialTheme(

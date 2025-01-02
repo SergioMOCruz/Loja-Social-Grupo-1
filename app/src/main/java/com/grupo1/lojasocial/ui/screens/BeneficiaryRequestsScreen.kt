20091b79f3610f1e8dc3ba49b6f42688e5b746b3
Zé Castro <a20351@alunos.ipca.pt>
2025-01-02 14:55:40 +0000

    add new request done

--- app/src/main/java/com/grupo1/lojasocial/ui/screens/BeneficiaryRequestsScreen.kt
+++ app/src/main/java/com/grupo1/lojasocial/ui/screens/BeneficiaryRequestsScreen.kt
@@ -1,6 +1,8 @@
 package com.grupo1.lojasocial.ui.screens
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,12 +14,15 @@ import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.SnackbarHost
 import androidx.compose.material.SnackbarHostState
+import androidx.compose.material.TextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
@@ -27,6 +32,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.grupo1.lojasocial.domain.model.Product
 import com.grupo1.lojasocial.ui.components.utils.requests.RequestItem
 import com.grupo1.lojasocial.ui.screens.header.SubHeaderScreen
 import com.grupo1.lojasocial.viewmodel.RequestsViewModel
@@ -42,6 +48,11 @@ fun BeneficiaryRequestsScreen(
     val beneficiaryName = navBackStackEntry?.arguments?.getString("beneficiaryName")
 
     val requests by requestsViewModel.request.collectAsState()
+
+    val (isCreating, setCreating) = remember { mutableStateOf(false) }
+    val (description, setDescription) = remember { mutableStateOf("") }
+    val (quantity, setQuantity) = remember { mutableStateOf("") }
+    val productList = remember { mutableStateListOf<Product>() }
 
     LaunchedEffect(key1 = profileId) {
         if (profileId != null) {
@@ -69,15 +80,84 @@ fun BeneficiaryRequestsScreen(
             modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
         )
 
-        Button(
-            onClick = { /*TODO*/ },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            shape = RoundedCornerShape(8.dp),
-            colors = ButtonDefaults.buttonColors(backgroundColor = Color.DarkGray, contentColor = Color.White)
-        ) {
-            Text(text = "Novo Pedido", color = Color.White)
+        if (!isCreating) {
+            Button(
+                onClick = { setCreating(true) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color.DarkGray, contentColor = Color.White)
+            ) {
+                Text(text = "Novo Pedido", color = Color.White)
+            }
+        } else {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                TextField(
+                    value = description,
+                    onValueChange = { setDescription(it) },
+                    label = { Text("Descrição") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                TextField(
+                    value = quantity,
+                    onValueChange = { setQuantity(it) },
+                    label = { Text("Quantidade") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+                    Button(
+                        shape = RoundedCornerShape(4.dp),
+                        colors = ButtonDefaults.buttonColors(backgroundColor = Color.DarkGray),
+                        onClick = {
+                        if (description.isNotEmpty() && quantity.isNotEmpty()) {
+                            productList.add(Product(description, quantity.toInt()))
+                        }
+                        setDescription("")
+                        setQuantity("")
+                    }) {
+                        Text(
+                            "Adicionar",
+                            color = Color.White
+                        )
+                    }
+                    Button(
+                        shape = RoundedCornerShape(4.dp),
+                        colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+                        onClick = { setCreating(false) }
+                    ) {
+                        Text(
+                            "Cancelar",
+                            color = Color.Red
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = RoundedCornerShape(4.dp),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.DarkGray),
+                    onClick = {
+                    if (description.isNotEmpty() && quantity.isNotEmpty()) {
+                        productList.add(Product(description, quantity.toInt()))
+                    }
+                    requestsViewModel.registerRequest(profileId!!, productList)
+                    productList.clear()
+                    requestsViewModel.getRequests(profileId)
+                    setCreating(false)
+                }) {
+                    Text(
+                        "Registar Pedido",
+                        color = Color.White
+                    )
+                }
+            }
         }
 
         val snackbarHostState = remember { SnackbarHostState() }
